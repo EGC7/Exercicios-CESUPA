99c7a68e8a760a28b6b7d0bd3aa1a1359cbd45a8
EGC7 <139312907+EGC7@users.noreply.github.com>
2026-03-13 09:34:14 -0300

    Finished Game 1 && Inicialized Game 2

--- Sistema de Jogo/SystemGame.c
+++ Sistema de Jogo/SystemGame.c
@@ -32,13 +32,25 @@ void showMenu(){
 	printf("\n");
 
 	printf("\n 1 - Perguntas e Respostas");
-	printf("\n 2 - Jogo nao sei o que");
+	printf("\n 2 - Cobra na Caixa!");
 	printf("\n 3 - Gousmas War");
 	printf("\n 4 - Sair");
 	
 	printf("\n >>> Escolha: ");
 	scanf("%d", &resp);
 	
+}
+
+void again(){
+	int i;
+	printf(">> Voce deseja jogar novamente?\n");
+	for (i=0;i<pts;i++) printf("-");
+	printf(">  1 - S || 0 - N <");
+	for (i=0;i<pts;i++) printf("-");
+	printf("\n");
+
+	printf(">>> Escolha: ");
+	scanf("%d", &resp);
 }
 
 void game1(){
@@ -53,10 +65,13 @@ void game1(){
 	printf("> Perguntas e Respostas <");
 	for (i=0;i<pts;i++) printf("-");
 	printf("\n");
+	printf(">> Neste jogo, o computador apresenta uma pergunta com quatro alternativas e\nsolicita que o usuario escolha a resposta correta. Apos a selecao, o sistema\nindicara se a resposta esta correta ou incorreta. Caso seja incorreta, sera\nexibida a resposta correta.\n");
+	for (i=0;i<45;i++) printf("-");
+	printf("\n");
 
-	printf("\nQuestion 01");
-	printf("\n 1. A - Resp 1\t|  2. B - Resp 2");
-	printf("\n 3. C - Resp 3\t|  4. D - Resp 4");
+	printf("\n>> Q01: Qual o planeta mais proximo do Sol?");
+	printf("\n 1. A - Netuno\t|  2. B - Terra");
+	printf("\n 3. C - Jupiter\t|  4. D - Mercurio");
 	printf("\n >>> Resposta: ");
 	scanf("%d", &esco);
 	switch(esco){
@@ -66,14 +81,14 @@ void game1(){
 			break;
 		case 1:case 2:case 3:
 		default:
-			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '4. D - Resp 4'\n");
+			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '4. D - Mercurio'\n");
 			break;
 	}
 	printf("\n");
 
-	printf("\nQuestion 02");
-	printf("\n 1. A - Resp 1\t|  2. B - Resp 2");
-	printf("\n 3. C - Resp 3\t|  4. D - Resp 4");
+	printf("\n>> Q02: A que temperatura a agua ferve?");
+	printf("\n 1. A - -10 Graus Celsius\t|  2. B - 0 Graus Celsius");
+	printf("\n 3. C - 100 Graus Celsius\t|  4. D - 67 Graus Celsius");
 	printf("\n >>> Resposta: ");
 	scanf("%d", &esco);
 	switch(esco){
@@ -83,14 +98,14 @@ void game1(){
 			break;
 		case 1:case 2:case 4:
 		default:
-			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '3. C - Resp 3'\n");
+			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '3. C - 100 Graus Celsius'\n");
 			break;
 	}
 	printf("\n");
 
-	printf("\nQuestion 03");
-	printf("\n 1. A - Resp 1\t|  2. B - Resp 2");
-	printf("\n 3. C - Resp 3\t|  4. D - Resp 4");
+	printf("\n>> Q03: Qual o maior planeta do sistema solar?");
+	printf("\n 1. A - Jupiter\t|  2. B - Saturno");
+	printf("\n 3. C - Plutao\t|  4. D - Venus");
 	printf("\n >>> Resposta: ");
 	scanf("%d", &esco);
 	switch(esco){
@@ -100,14 +115,14 @@ void game1(){
 			break;
 		case 2:case 3:case 4:
 		default:
-			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '1. A - Resp 1'\n");
+			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '1. A - Jupiter'\n");
 			break;
 	}
 	printf("\n");
 
-	printf("\nQuestion 04");
-	printf("\n 1. A - Resp 1\t|  2. B - Resp 2");
-	printf("\n 3. C - Resp 3\t|  4. D - Resp 4");
+	printf("\n>> Q04: Um anel tem 3 pedras preciosas. Quantas pedras preciosas tem 11 aneis?");
+	printf("\n 1. A - 30 Pedras\t|  2. B - 11 Pedras");
+	printf("\n 3. C - 90 Pedras\t|  4. D - 33 Pedras");
 	printf("\n >>> Resposta: ");
 	scanf("%d", &esco);
 	switch(esco){
@@ -117,14 +132,14 @@ void game1(){
 			break;
 		case 1:case 2:case 3:
 		default:
-			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '4. D - Resp 4'\n");
+			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '4. D - 33 Pedras'\n");
 			break;
 	}
 	printf("\n");
 
-	printf("\nQuestion 05");
-	printf("\n 1. A - Resp 1\t|  2. B - Resp 2");
-	printf("\n 3. C - Resp 3\t|  4. D - Resp 4");
+	printf("\n>> Q05: Quantos continentes existem?");
+	printf("\n 1. A - 10 Continentes\t|  2. B - 06 Continentes");
+	printf("\n 3. C - 4 Continentes\t|  4. D - 08 Continentes");
 	printf("\n >>> Resposta: ");
 	scanf("%d", &esco);
 	switch(esco){
@@ -134,7 +149,7 @@ void game1(){
 			break;
 		case 1:case 3:case 4:
 		default:
-			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '2. B - Resp 2'\n");
+			printf(">> Parece que voce errou :(\n>> A Resposta correta era: '2. B - 06 Continentes'\n");
 			break;
 	}
 	printf("\nSua Pontucao Foi"); pontosBonitoskkkj(5);
@@ -142,14 +157,82 @@ void game1(){
 	Sleep(1500);
 	system("cls"); 
 	
-	printf("Voce deseja jogar novamente?\n");
+	again();
+}
+
+void game2(){
+	int i, posSnake, posButton, box[7], ln, cl, jgd;
+	posSnake = 0;
+	posButton = 0;
+	
+	system("cls");
+	printf(">> Iniciando 'Cobra na Caixa!'");
+	pontosBonitoskkkj(5);
+	system("cls");
+	
 	for (i=0;i<pts;i++) printf("-");
-	printf(">  1 - S || 0 - N <");
+	printf("> Cobra na Caixa! <");
 	for (i=0;i<pts;i++) printf("-");
 	printf("\n");
+	printf(">> A historia do jogo se passa dentro de uma tumba egipcia onde dois\nexploradores ficaram presos. No centro da sala, ha cinco caixas: uma\ndelas contem o botao para abrir a porta, enquanto uma outra esconde\numa cobra mortal. A cada rodada, o local do botao e da cobra muda de\ncaixa de forma aleatoria, aumentando o desafio e a imprevisibilidade\ndo jogo.\n");
+	for (i=0;i<39;i++) printf("-");
+	printf("\n\n");
+	
+	jgd = (( rand() % (2 - 1 + 1) ) + 1);
+	
+	if ( jgd == 2) printf(">> Jogador 2 Comeca!");
+	else printf("Jogador 1 Comeca!");
+	pontosBonitoskkkj(5);
+	printf("\n\n");
 
-	printf(">>> Escolha: ");
-	scanf("%d", &resp);
+	printf("    1    2    3\n1.\t");
+	for (i=0; i<7; i++){
+		box[i]=0;
+		printf("[ ]  ");
+		if ( (i%3==0) && (i!=6) ) printf("\n%d. ", (i/3)+2);
+	}
+	printf("\n");
+	posSnake = ( rand() % (6 - 0 + 1) ) + 0;
+	posButton = ( rand() % (6 - 0 + 1 ) ) + 0;
+	while ( posSnake == posButton ) posButton = ( rand() % (6 - 0 + 1 ) ) + 0;	
+	
+	box[posSnake] = 2;
+	box[posButton] = 1;
+
+	printf("\n\n");
+	for (i = 0; i < 7; i++) printf("%d", box[i]);
+	printf("\n\n");
+
+	printf(">> Selecione uma caixa digitando sua linha e depois sua coluna, respectivamente.\n\n");
+	printf(">>> Linha: ");
+	scanf("%d", &ln);
+	
+	while (true){
+		if (box[ln] == 2){
+			if (jgd == 1) printf(">> Jogador 1 Perdeu!");
+			else printf(">> Jogador 2 Perdeu!");
+		} else if (box)
+		
+	}
+
+	if (ln == 1){
+		if (box[0] == 2) printf("Snake!!");
+		else if (box[0] == 1) printf("Botao!!");
+		else printf("Caixa Vazia.");
+	} 
+	else{
+		printf(">>> Coluna: ");
+		scanf("%d", &cl);
+	}
+	
+	
+	scanf("%d");
+		
+	
+
+	again();
+	if (resp == 1) resp = 2;	
+
 }
 
 int main (void){
@@ -165,10 +248,16 @@ int main (void){
 		
 		switch(resp)
 		{
+		case 0:
+			printf("\nVoltando para o Menu"); pontosBonitoskkkj(5);
+			Sleep(700);
+			system("cls");
+			showMenu();
+			break;
 		case 1:
 			game1(pts); break;
 		case 2:
-			exitGame(); break;
+			game2(); break;
 		case 3:
 			exitGame(); break;
 		case 4:
@@ -176,7 +265,7 @@ int main (void){
 		default:
 			system("cls");
 			printf(">> Perdao, nao encontramos esta opcao. Tente digitar um dos valores do menu.\n\n");
-			showMenu(10);
+			showMenu();
 			break;
 		}
 	}
